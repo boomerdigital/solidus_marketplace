4569fc4c6835a4b8c24102f4f22989d635fb6c8f
Gonzalo Moreno <gmoreno@acid.cl>
2016-05-09 00:49:13 -0300

    fix annoying and unfixed bugs

--- app/models/spree/supplier_ability.rb
+++ app/models/spree/supplier_ability.rb
@@ -9,10 +9,10 @@ module Spree
         # TODO: Want this to be inline like:
         # can [:admin, :read, :stock], Spree::Product, suppliers: { id: user.supplier_id }
         # can [:admin, :read, :stock], Spree::Product, supplier_ids: user.supplier_id
-        can [:admin, :read, :stock], Spree::Product do |product|
+        can [:admin, :manage, :stock], Spree::Product do |product|
           product.supplier_ids.include?(user.supplier_id)
         end
-        can [:admin, :index], Spree::Product
+        can [:admin, :create, :index], Spree::Product
         can [:admin, :manage, :read, :ready, :ship], Spree::Shipment, order: { state: 'complete' }, stock_location: { supplier_id: user.supplier_id }
         can [:admin, :create, :update], :stock_items
         can [:admin, :manage], Spree::StockItem, stock_location_id: user.supplier.stock_locations.pluck(:id)
