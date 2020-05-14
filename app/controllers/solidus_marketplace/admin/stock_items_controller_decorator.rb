78ef1c1efa1e5e2ef94083cc175bba9c771df931
Edwin Cruz <edwin.cruz@magmalabs.io>
2020-05-14 17:18:13 -0500

    Suppliers should only see their variants in admin
    When restocking products, suppliers should only see
    their variants improving the experience of managing
    inventory

--- app/controllers/solidus_marketplace/admin/stock_items_controller_decorator.rb
+++ app/controllers/solidus_marketplace/admin/stock_items_controller_decorator.rb
@@ -15,6 +15,14 @@ module SolidusMarketplace
             @stock_item_stock_locations = params[:stock_location_id].present? ? @stock_locations.where(id: params[:stock_location_id]) : @stock_locations
           end
         end
+
+        def variant_scope
+          scope = super
+          if try_spree_current_user.supplier
+            scope = scope.joins(:stock_locations).where(spree_stock_locations: {supplier_id: spree_current_user.supplier.id})
+          end
+          scope
+        end
       end
     end
   end
